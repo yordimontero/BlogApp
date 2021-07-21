78e61803fbfe5a064520c9047b0e54ecc8c8ea15
Yordi Montero <monterocalvoyordi@gmail.com>
2021-07-21 14:44:03 -0600

    PostItemView for HomeRecyclerView. HomeScreenAdapter.

---
+++ app/src/main/java/com/circleappsstudio/blogapp/ui/home/HomeScreenFragment.kt
@@ -0,0 +1,17 @@
+package com.circleappsstudio.blogapp.ui.home
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.View
+import com.circleappsstudio.blogapp.R
+
+class HomeScreenFragment : Fragment(R.layout.fragment_home_screen) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+
+    }
+
+}
---
+++ app/src/main/java/com/circleappsstudio/blogapp/ui/home/adapter/HomeScreenAdapter.kt
@@ -0,0 +1,65 @@
+package com.circleappsstudio.blogapp.ui.home.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.circleappsstudio.blogapp.core.BaseViewHolder
+import com.circleappsstudio.blogapp.data.model.Post
+import com.circleappsstudio.blogapp.databinding.PostItemViewBinding
+
+class HomeScreenAdapter(
+    private val listPost: List<Post>
+): RecyclerView.Adapter<BaseViewHolder<*>>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BaseViewHolder<*> {
+
+        // itemBinding inflates the XML view
+        val itemBinding = PostItemViewBinding.inflate(
+            LayoutInflater.from(parent.context),
+            parent,
+            false
+        )
+
+        /**
+            onCreateViewHolder creates the instance of HomeScreenViewHolder() that gives
+            as parameters the inflated XML view and the context from parent.
+         */
+        return HomeScreenViewHolder(itemBinding, parent.context)
+    }
+
+    override fun onBindViewHolder(holder: BaseViewHolder<*>, position: Int) {
+
+        when(holder) {
+            is HomeScreenViewHolder -> {
+                holder.bind(listPost[position])
+            }
+        }
+
+    }
+
+    override fun getItemCount(): Int = listPost.size
+
+    /**
+        HomeScreenViewHolder() extends from BaseViewHolder<> that
+        needs a generic value "T" (what is going to "inflate").
+
+        The function bind() overrides "T" generic with the value passed when BaseViewHolder<>
+        is extended ("Post" in this case).
+    */
+    private inner class HomeScreenViewHolder(
+        val binding: PostItemViewBinding,
+        val context: Context
+    ): BaseViewHolder<Post>(binding.root) {
+
+        override fun bind(item: Post) {
+            Glide.with(context).load(item.profile_picture).centerCrop().into(binding.profilePicture)
+            binding.profileName.text = item.profile_name
+            binding.postTimestamp.text = "Testing"
+            Glide.with(context).load(item.post_image).centerCrop().into(binding.postImage)
+        }
+
+    }
+
+}
